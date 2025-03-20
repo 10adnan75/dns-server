071fa48972efd83ffb4f29c891a1f2b13a539e1b
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2025-03-20 04:38:04 -0700

    parse compressed packet

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -5,6 +5,8 @@ import java.net.DatagramPacket;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Parser implements IDNSMessage {
 
@@ -26,26 +28,96 @@ public class Parser implements IDNSMessage {
     public byte[] parse(DatagramPacket packet) throws IOException {
 
         ByteBuffer DNSMessage = ByteBuffer.allocate(512);
-        DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(packet.getData()));
+        byte[] data = packet.getData();
+        int packetLength = packet.getLength();
+        DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(data, 0, packetLength));
 
         short ID = dataInputStream.readShort();
-        short LINE = dataInputStream.readShort();
-        short OP_CODE = (short) ((LINE >> 11) & 0b1111);
-        boolean RD = ((LINE >> 8) & 0b1) == 1;
-        short R_CODE = OP_CODE != 0 ? (short) 4 : (short) 0;
-        short FLAGS = (short) (0x8000 | (OP_CODE << 11) | (RD ? 0x0100 : 0) | R_CODE);
+        short FLAGS = dataInputStream.readShort();
         short QD_COUNT = dataInputStream.readShort();
-        short AN_COUNT = dataInputStream.readShort();
+        short AN_COUNT = (short) (dataInputStream.readShort() | QD_COUNT);
         short NS_COUNT = dataInputStream.readShort();
         short AR_COUNT = dataInputStream.readShort();
 
+        FLAGS = (short) (FLAGS | 0x8000);
+
+        List<String> domainNames = new ArrayList<>();
+        List<Short> types = new ArrayList<>();
+        List<Short> classes = new ArrayList<>();
+
+        int headerSize = 12;
+        for (int i = 0; i < QD_COUNT; i++) {
+
+            String domainName = readDomainName(data, dataInputStream, headerSize);
+            short TYPE = dataInputStream.readShort();
+            short CLASS = dataInputStream.readShort();
+
+            domainNames.add(domainName);
+            types.add(TYPE);
+            classes.add(CLASS);
+
+        }
+
+        new Header(ID, FLAGS, QD_COUNT, AN_COUNT, NS_COUNT, AR_COUNT).addHeader(DNSMessage);
+
+        for (int i = 0; i < domainNames.size(); i++) {
+
+            new Question(domainNames.get(i), types.get(i), classes.get(i)).addQuestion(DNSMessage);
+
+        }
+
+        for (int i = 0; i < domainNames.size(); i++) {
+
+            byte[] ipAddress = generateIPAddress(domainNames.get(i));
+            new Answer(
+                    domainNames.get(i),
+                    types.get(i),
+                    classes.get(i),
+                    60,
+                    (short) 4,
+                    ipAddress).addAnswer(DNSMessage);
+
+        }
+
+        byte[] response = new byte[DNSMessage.position()];
+        DNSMessage.flip();
+        DNSMessage.get(response);
+
+        return response;
+
+    }
+
+    private String readDomainName(byte[] data, DataInputStream input, int baseOffset) throws IOException {
+
         StringBuilder domainName = new StringBuilder();
-        int length;
+        boolean jumped = false;
+        int savedPosition = 0;
 
-        while ((length = dataInputStream.readByte() & 0xFF) > 0) {
+        while (true) {
 
-            byte[] label = new byte[length];
-            dataInputStream.readFully(label, 0, length);
+            int length = input.readUnsignedByte();
+
+            if ((length & 0xC0) == 0xC0) {
+
+                if (!jumped) {
+
+                    savedPosition = input.available();
+                    jumped = true;
+
+                }
+
+                int offset = ((length & 0x3F) << 8) | (input.readUnsignedByte() & 0xFF);
+                ByteArrayInputStream tempStream = new ByteArrayInputStream(data, offset, data.length - offset);
+                input = new DataInputStream(tempStream);
+                continue;
+
+            }
+
+            if (length == 0) {
+
+                break;
+
+            }
 
             if (domainName.length() > 0) {
 
@@ -53,32 +125,36 @@ public class Parser implements IDNSMessage {
 
             }
 
+            byte[] label = new byte[length];
+            input.readFully(label);
             domainName.append(new String(label));
 
         }
 
-        String NAME = domainName.toString();
-        short TYPE = dataInputStream.readShort();
-        short CLASS = dataInputStream.readShort();
+        if (jumped) {
 
-        byte[] DATA = lookupIPAddress(NAME, TYPE);
-        short RD_LENGTH = (short) (DATA.length);
-        AN_COUNT = DATA != null ? (short) 0x01 : (short) 0x00;
-        int TTL = 60;
+            ByteArrayInputStream restoreStream = new ByteArrayInputStream(
+                    data,
+                    data.length - savedPosition,
+                    savedPosition);
+            input = new DataInputStream(restoreStream);
 
-        new Header(ID, FLAGS, QD_COUNT, AN_COUNT, NS_COUNT, AR_COUNT).addHeader(DNSMessage);
+        }
 
-        new Question(NAME, TYPE, CLASS).addQuestion(DNSMessage);
-
-        new Answer(NAME, TYPE, CLASS, TTL, RD_LENGTH, DATA).addAnswer(DNSMessage);
-
-        return DNSMessage.array();
+        return domainName.toString();
 
     }
 
-    private byte[] lookupIPAddress(String domainName, short recordType) {
+    private byte[] generateIPAddress(String domainName) {
 
-        return new byte[] { 0x08, 0x08, 0x08, 0x08 };
+        int hash = Math.abs(domainName.hashCode());
+
+        return new byte[] {
+                (byte) 192,
+                (byte) 168,
+                (byte) (hash % 256),
+                (byte) ((hash >> 8) % 256)
+        };
 
     }
 
